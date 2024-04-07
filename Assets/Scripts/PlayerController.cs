df0ef3c43df9e7a705402ef396360bc51e1400ed
soosung03 <yss030896@gmail.com>
2024-04-06 17:12:56 -0700

    done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+   private Rigidbody rb; 
+
+   private int count;
+
+   private float movementX;
+   private float movementY;
+
+   public float speed = 0;
+
+   public TextMeshProUGUI countText;
+
+   public GameObject winTextObject;
+
+   private int JumpCount = 0;
+
+   void Start()
+   {
+      rb = GetComponent<Rigidbody>();
+
+      count = 0;
+
+      SetCountText();
+
+      winTextObject.SetActive(false);
+   }
+   
+   void OnMove(InputValue movementValue)
+   {
+      Vector2 movementVector = movementValue.Get<Vector2>();
+
+      movementX = movementVector.x; 
+      movementY = movementVector.y; 
+   }
+
+   void OnJump(InputValue jumpValue) 
+   {
+      if (JumpCount < 2)
+      {
+         rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+         JumpCount++;
+      }
+   }
+
+   void OnCollisionEnter(Collision other)
+   {
+      JumpCount = 0;
+   }
+
+   private void FixedUpdate() 
+   {
+      Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+      rb.AddForce(movement * speed); 
+   }
+   
+   void OnTriggerEnter(Collider other) 
+   {
+      if (other.gameObject.CompareTag("PickUp")) 
+      {
+         other.gameObject.SetActive(false);
+
+         count = count + 1;
+
+         SetCountText();
+      }
+   }
+
+   void SetCountText() 
+   {
+      countText.text = "Count: " + count.ToString();
+
+      if (count >= 8)
+      {
+         winTextObject.SetActive(true);
+      }
+   }
+}
